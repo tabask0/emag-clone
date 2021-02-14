d3cde8337d2db2d26b72367a439a72d60fd1b9d3
tabasko <bucur37@gmail.com>
2021-02-14 21:07:12 +0200

    added categories

---
+++ src/components/Services.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+      <h1>Services works!</h1>
+    </div>
+  )
+}
+
+export default Services
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Categories from './components/Categories'
+import Services from './components/Services';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Categories />
+    <Services /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
